87e6f04f75e3429a5ca7f304502e924eca14fdbf
baofan <baofanyf@jftech.com>
2023-11-30 15:46:27 +0800

    feat: 删除不支持的api

--- ios/Classes/PlainNotificationTokenPlugin.m
+++ ios/Classes/PlainNotificationTokenPlugin.m
@@ -6,12 +6,12 @@
     FlutterMethodChannel *_channel;
 }
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"plain_notification_token"
-            binaryMessenger:[registrar messenger]];
-  PlainNotificationTokenPlugin* instance = [[PlainNotificationTokenPlugin alloc] initWithChannel:channel];
-  [registrar addApplicationDelegate:instance];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"plain_notification_token"
+                                     binaryMessenger:[registrar messenger]];
+    PlainNotificationTokenPlugin* instance = [[PlainNotificationTokenPlugin alloc] initWithChannel:channel];
+    [registrar addApplicationDelegate:instance];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (instancetype)initWithChannel:(FlutterMethodChannel *)channel {
@@ -22,29 +22,27 @@
         dispatch_async(dispatch_get_main_queue(), ^() {
             [[UIApplication sharedApplication] registerForRemoteNotifications];
         });
-        if (@available(iOS 10.0, *)) {
-            [[UNUserNotificationCenter currentNotificationCenter] getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
-                NSDictionary *settingsDictionary = @{
-                                                     @"sound" : [NSNumber numberWithBool:settings.soundSetting == UNNotificationSettingEnabled],
-                                                     @"badge" : [NSNumber numberWithBool:settings.badgeSetting == UNNotificationSettingEnabled],
-                                                     @"alert" : [NSNumber numberWithBool:settings.alertSetting == UNNotificationSettingEnabled],
-                                                     };
-                [self->_channel invokeMethod:@"onIosSettingsRegistered" arguments:settingsDictionary];
-            }];
-        }
+        [[UNUserNotificationCenter currentNotificationCenter] getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+            NSDictionary *settingsDictionary = @{
+                @"sound" : [NSNumber numberWithBool:settings.soundSetting == UNNotificationSettingEnabled],
+                @"badge" : [NSNumber numberWithBool:settings.badgeSetting == UNNotificationSettingEnabled],
+                @"alert" : [NSNumber numberWithBool:settings.alertSetting == UNNotificationSettingEnabled],
+            };
+            [self->_channel invokeMethod:@"onIosSettingsRegistered" arguments:settingsDictionary];
+        }];
     }
     return self;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getToken" isEqualToString:call.method]) {
-    result([self getToken]);
-  } else if ([@"requestPermission" isEqualToString:call.method]) {
-      [self requestPermissionWithSettings:[call arguments]];
-      result(nil);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"getToken" isEqualToString:call.method]) {
+        result([self getToken]);
+    } else if ([@"requestPermission" isEqualToString:call.method]) {
+        [self requestPermissionWithSettings:[call arguments]];
+        result(nil);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 - (NSString *)getToken {
@@ -52,60 +50,34 @@
 }
 
 - (void)requestPermissionWithSettings: (NSDictionary<NSString*, NSNumber*> *)settings {
-    if (@available(iOS 10.0, *)) {
-        UNAuthorizationOptions options = UNAuthorizationOptionNone;
-        if ([[settings objectForKey:@"sound"] boolValue]) {
-            options |= UNAuthorizationOptionSound;
-        }
-        if ([[settings objectForKey:@"badge"] boolValue]) {
-            options |= UNAuthorizationOptionBadge;
-        }
-        if ([[settings objectForKey:@"alert"] boolValue]) {
-            options |= UNAuthorizationOptionAlert;
-        }
-        [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:options completionHandler:^(BOOL granted, NSError * _Nullable error) {
-            if (error != nil) {
-                NSLog(@"Error during requesting notification permission: %@", error);
-            }
-            if (granted) {
-                dispatch_async(dispatch_get_main_queue(), ^() {
-                    [[UIApplication sharedApplication] registerForRemoteNotifications];
-                });
-                [self->_channel invokeMethod:@"onIosSettingsRegistered" arguments:settings];
-            } else {
-                NSNumber* falseNumber = [NSNumber numberWithBool: NO];
-                NSDictionary<NSString*, NSNumber*> *empty = [NSDictionary dictionaryWithObjectsAndKeys: falseNumber, @"badge", falseNumber, @"alert", falseNumber, @"sound", nil];
-                [self->_channel invokeMethod:@"onIosSettingsRegistered" arguments:empty];
-            }
-        }];
+    UNAuthorizationOptions options = UNAuthorizationOptionNone;
+    if ([[settings objectForKey:@"sound"] boolValue]) {
+        options |= UNAuthorizationOptionSound;
     }
-    else {
-        UIUserNotificationType types = 0;
-        if ([[settings objectForKey:@"sound"] boolValue]) {
-            types |= UIUserNotificationTypeSound;
-        }
-        if ([[settings objectForKey:@"badge"] boolValue]) {
-            types |= UIUserNotificationTypeBadge;
-        }
-        if ([[settings objectForKey:@"alert"] boolValue]) {
-            types |= UIUserNotificationTypeAlert;
-        }
-        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
-        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    if ([[settings objectForKey:@"badge"] boolValue]) {
+        options |= UNAuthorizationOptionBadge;
     }
+    if ([[settings objectForKey:@"alert"] boolValue]) {
+        options |= UNAuthorizationOptionAlert;
+    }
+    [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:options completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (error != nil) {
+            NSLog(@"Error during requesting notification permission: %@", error);
+        }
+        if (granted) {
+            dispatch_async(dispatch_get_main_queue(), ^() {
+                [[UIApplication sharedApplication] registerForRemoteNotifications];
+            });
+            [self->_channel invokeMethod:@"onIosSettingsRegistered" arguments:settings];
+        } else {
+            NSNumber* falseNumber = [NSNumber numberWithBool: NO];
+            NSDictionary<NSString*, NSNumber*> *empty = [NSDictionary dictionaryWithObjectsAndKeys: falseNumber, @"badge", falseNumber, @"alert", falseNumber, @"sound", nil];
+            [self->_channel invokeMethod:@"onIosSettingsRegistered" arguments:empty];
+        }
+    }];
 }
 
 #pragma mark - AppDelegate
-
-- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
-    NSDictionary *settingsDictionary = @{
-                                         @"sound" : [NSNumber numberWithBool:notificationSettings.types & UIUserNotificationTypeSound],
-                                         @"badge" : [NSNumber numberWithBool:notificationSettings.types & UIUserNotificationTypeBadge],
-                                         @"alert" : [NSNumber numberWithBool:notificationSettings.types & UIUserNotificationTypeAlert],
-                                         };
-    [_channel invokeMethod:@"onIosSettingsRegistered" arguments:settingsDictionary];
-}
-
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     NSString *tokenStr = [NSString stringWithFormat:@"%@",deviceToken];
     if ([tokenStr containsString:@"length"]) {
@@ -127,21 +99,21 @@
 
 //MARK: APP收到报警消息
 - (BOOL)application:(UIApplication*)application
-    didReceiveRemoteNotification:(NSDictionary*)userInfo
+didReceiveRemoteNotification:(NSDictionary*)userInfo
 fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler{
-//    NSDictionary *apsDic = userInfo[@"aps"];
-//    __block NSString *msgContent = apsDic[@"alert"];
-//
-//    //NSLog(@"cycy = %@",msgContent);
-//    //发送一条新消息的通知，相关界面收到通知后刷新界面
-//    //todo:推送服务器还需要新增这些字段才能完成消息计数数的功能
-//    NSString *alarmId = userInfo[@"ALARMID"];
-//    NSString *devID = userInfo[@"UUID"];
-//    NSString *subSN = userInfo[@"SUBSN"];
-//
-//    if (!devID) {
-//        return YES;
-//    }
+    //    NSDictionary *apsDic = userInfo[@"aps"];
+    //    __block NSString *msgContent = apsDic[@"alert"];
+    //
+    //    //NSLog(@"cycy = %@",msgContent);
+    //    //发送一条新消息的通知，相关界面收到通知后刷新界面
+    //    //todo:推送服务器还需要新增这些字段才能完成消息计数数的功能
+    //    NSString *alarmId = userInfo[@"ALARMID"];
+    //    NSString *devID = userInfo[@"UUID"];
+    //    NSString *subSN = userInfo[@"SUBSN"];
+    //
+    //    if (!devID) {
+    //        return YES;
+    //    }
     
     NSString *messageJsonStr = [self _toJsonStr: userInfo];
     NSLog(@"PlainNotificationTokenPlugin -- 原生收到推送消息: %@",messageJsonStr);
@@ -155,7 +127,6 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandl
 
 // 注册通知失败 处理方法
 -(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
-    
     NSLog(@"PlainNotificationTokenPlugin -- 原生注册推送失败");
 }
 
